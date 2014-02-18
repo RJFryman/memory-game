4728ca3651c9424d46fc20e80219f788c96f6851
Robert Fryman <robert.fryman@gmail.com>
2014-02-18 12:44:13 -0600

    add octocats.js

---
+++ app/js/source/octocats.js
@@ -0,0 +1,340 @@
+var octocats = new Array[];
+
+octocats[0]= new Image(75, 75);
+octocats[0].src = 'inded/media/yaktocat.png';
+
+octocats[1]= new Image(75, 75);
+octocats[1].src = 'index/media/Mardigrastocat.png';
+
+octocats[2]= new Image(75, 75);
+octocats[2].src = 'index/media/Professortocat_v2.png';
+
+octocats[3]= new Image(75, 75);
+octocats[3].src = 'index/media/Robotocat.png';
+
+octocats[4]= new Image(75, 75);
+octocats[4].src = 'index/media/adventure-cat.png';
+
+octocats[5]= new Image(75, 75);
+octocats[5].src = 'index/media/agendacat.png';
+
+octocats[6]= new Image(75, 75);
+octocats[6].src = 'index/media/andycat.jpg';
+
+octocats[7]= new Image(75, 75);
+octocats[7].src = 'index/media/baracktocat.jpg';
+
+octocats[8]= new Image(75, 75);
+octocats[8].src = 'index/media/bear-cavalry.png';
+
+octocats[9]= new Image(75, 75);
+octocats[9].src = 'index/media/benevocats.png';
+
+octocats[10]= new Image(75, 75);
+octocats[10].src = 'index/media/bouncercat.png';
+
+octocats[11]= new Image(75, 75);
+octocats[11].src = 'index/media/carlostocat.gif';
+
+octocats[12]= new Image(75, 75);
+octocats[12].src = 'index/media/chellocat.jpg';
+
+octocats[13]= new Image(75, 75);
+octocats[13].src = 'index/media/cherryontop-o-cat.png';
+
+octocats[14]= new Image(75, 75);
+octocats[14].src = 'index/media/class-act.png';
+
+octocats[15]= new Image(75, 75);
+octocats[15].src = 'index/media/cloud.jpg';
+
+octocats[16]= new Image(75, 75);
+octocats[16].src = 'index/media/codercat.jpg';
+
+octocats[17]= new Image(75, 75);
+octocats[17].src = 'index/media/collabocats.jpg';
+
+octocats[18]= new Image(75, 75);
+octocats[18].src = 'index/media/constructocat2.jpg';
+
+octocats[19]= new Image(75, 75);
+octocats[19].src = 'index/media/daftpunktocat-guy.gif';
+
+octocats[20]= new Image(75, 75);
+octocats[20].src = 'index/media/daftpunktocat-thomas.gif';
+
+octocats[21]= new Image(75, 75);
+octocats[21].src = 'index/media/deckfailcat.png';
+
+octocats[22]= new Image(75, 75);
+octocats[22].src = 'index/media/defunktocat.png';
+
+octocats[23]= new Image(75, 75);
+octocats[23].src = 'index/media/doctocat-brown.jpg';
+
+octocats[24]= new Image(75, 75);
+octocats[24].src = 'index/media/dodgetocat.jpg';
+
+octocats[25]= new Image(75, 75);
+octocats[25].src = 'index/media/dodgetocat_v2.png';
+
+octocats[26]= new Image(75, 75);
+octocats[26].src = 'index/media/dojocat.jpg';
+
+octocats[27]= new Image(75, 75);
+octocats[27].src = 'index/media/dolla-dolla-bill-yall.jpg';
+
+octocats[28]= new Image(75, 75);
+octocats[28].src = 'index/media/droctocat.png';
+
+octocats[29]= new Image(75, 75);
+octocats[29].src = 'index/media/droidtocat.png';
+
+octocats[30]= new Image(75, 75);
+octocats[30].src = 'index/media/drunktocat.jpg';
+
+octocats[31]= new Image(75, 75);
+octocats[31].src = 'index/media/drupalcat.jpg';
+
+octocats[32]= new Image(75, 75);
+octocats[32].src = 'index/media/dunetocat.png';
+
+octocats[33]= new Image(75, 75);
+octocats[33].src = 'index/media/electrocat.png';
+
+octocats[34]= new Image(75, 75);
+octocats[34].src = 'index/media/father_timeout.jpg';
+
+octocats[35]= new Image(75, 75);
+octocats[35].src = 'index/media/femalecodertocat.png';
+
+octocats[36]= new Image(75, 75);
+octocats[36].src = 'index/media/forktocat.jpg';
+
+octocats[37]= new Image(75, 75);
+octocats[37].src = 'index/media/founding-father.jpg';
+
+octocats[38]= new Image(75, 75);
+octocats[38].src = 'index/media/foundingfather_v2.png';
+
+octocats[39]= new Image(75, 75);
+octocats[39].src = 'index/media/front-end-conftocat.png';
+
+octocats[40]= new Image(75, 75);
+octocats[40].src = 'index/media/gangnamtocat.png';
+
+octocats[41]= new Image(75, 75);
+octocats[41].src = 'index/media/goretocat.png';
+
+octocats[42]= new Image(75, 75);
+octocats[42].src = 'index/media/grim-repo.jpg';
+
+octocats[43]= new Image(75, 75);
+octocats[43].src = 'index/media/grinchtocat.gif';
+
+octocats[44]= new Image(75, 75);
+octocats[44].src = 'index/media/heisencat.png';
+
+octocats[45]= new Image(75, 75);
+octocats[45].src = 'index/media/herme-t-crabb.png';
+
+octocats[46]= new Image(75, 75);
+octocats[46].src = 'index/media/hipster-partycat.jpg';
+
+octocats[47]= new Image(75, 75);
+octocats[47].src = 'index/media/homercat.png';
+
+octocats[48]= new Image(75, 75);
+octocats[48].src = 'index/media/hubot.jpg';
+
+octocats[49]= new Image(75, 75);
+octocats[49].src = 'index/media/inspectocat.jpg';
+
+octocats[50]= new Image(75, 75);
+octocats[50].src = 'index/media/ironcat.jpg';
+
+octocats[51]= new Image(75, 75);
+octocats[51].src = 'index/media/jean-luc-picat.jpg';
+
+octocats[52]= new Image(75, 75);
+octocats[52].src = 'index/media/jenktocat.jpg';
+
+octocats[53]= new Image(75, 75);
+octocats[53].src = 'index/media/kimonotocat.png';
+
+octocats[54]= new Image(75, 75);
+octocats[54].src = 'index/media/labtocat.png';
+
+octocats[55]= new Image(75, 75);
+octocats[55].src = 'index/media/linktocat.jpg';
+
+octocats[56]= new Image(75, 75);
+octocats[56].src = 'index/media/maxtocat.gif';
+
+octocats[57]= new Image(75, 75);
+octocats[57].src = 'index/media/megacat-2.png';
+
+octocats[58]= new Image(75, 75);
+octocats[58].src = 'index/media/megacat.jpg';
+
+octocats[59]= new Image(75, 75);
+octocats[59].src = 'index/media/minion.png';
+
+octocats[60]= new Image(75, 75);
+octocats[60].src = 'index/media/momtocat.png';
+
+octocats[61]= new Image(75, 75);
+octocats[61].src = 'index/media/monroe.jpg';
+
+octocats[62]= new Image(75, 75);
+octocats[63].src = 'index/media/motherhubbertocat.png';
+
+octocats[63]= new Image(75, 75);
+octocats[63].src = 'index/media/mummytocat.gif';
+
+octocats[64]= new Image(75, 75);
+octocats[64].src = 'index/media/murakamicat.png';
+
+octocats[65]= new Image(75, 75);
+octocats[65].src = 'index/media/notocat.jpg';
+
+octocats[66]= new Image(75, 75);
+octocats[66].src = 'index/media/nyantocat.gif';
+
+octocats[67]= new Image(75, 75);
+octocats[67].src = 'index/media/octdrey-catburn.jpg';
+
+octocats[68]= new Image(75, 75);
+octocats[68].src = 'index/media/octobiwan.jpg';
+
+octocats[69]= new Image(75, 75);
+octocats[69].src = 'index/media/octocat-de-los-muertos.jpg';
+
+octocats[70]= new Image(75, 75);
+octocats[70].src = 'index/media/octoclark-kentocat.jpg';
+
+octocats[71]= new Image(75, 75);
+octocats[71].src = 'index/media/octofez.png';
+
+octocats[72]= new Image(75, 75);
+octocats[72].src = 'index/media/octoliberty.png';
+
+octocats[73]= new Image(75, 75);
+octocats[73].src = 'index/media/octonaut.jpg';
+
+octocats[74]= new Image(75, 75);
+octocats[74].src = 'index/media/octotron.jpg';
+
+octocats[75]= new Image(75, 75);
+octocats[75].src = 'index/media/okal-eltocat.jpg';
+
+octocats[76]= new Image(75, 75);
+octocats[76].src = 'index/media/oktobercat.png';
+
+octocats[77]= new Image(75, 75);
+octocats[77].src = 'index/media/orderedlistocat.png';
+
+octocats[78]= new Image(75, 75);
+octocats[78].src = 'index/media/original.png';
+
+octocats[79]= new Image(75, 75);
+octocats[79].src = 'index/media/pacman-ghosts.jpg';
+
+octocats[80]= new Image(75, 75);
+octocats[80].src = 'index/media/plumber.jpg';
+
+octocats[81]= new Image(75, 75);
+octocats[81].src = 'index/media/poptocat.png';
+
+octocats[82]= new Image(75, 75);
+octocats[82].src = 'index/media/poptocat_v2.png';
+
+octocats[83]= new Image(75, 75);
+octocats[83].src = 'index/media/puppeteer.png';
+
+octocats[84]= new Image(75, 75);
+octocats[84].src = 'index/media/pusheencat.png';
+
+octocats[85]= new Image(75, 75);
+octocats[85].src = 'index/media/pythocat.png';
+
+octocats[86]= new Image(75, 75);
+octocats[86].src = 'index/media/red-polo.png';
+
+octocats[87]= new Image(75, 75);
+octocats[87].src = 'index/media/repo.png';
+
+octocats[88]= new Image(75, 75);
+octocats[88].src = 'index/media/riddlocat.png';
+
+octocats[89]= new Image(75, 75);
+octocats[89].src = 'index/media/saint-nicktocat.jpg';
+
+octocats[90]= new Image(75, 75);
+octocats[90].src = 'index/media/scarletteocat.jpg';
+
+octocats[91]= new Image(75, 75);
+octocats[91].src = 'index/media/scottocat.jpg';
+
+octocats[92]= new Image(75, 75);
+octocats[92].src = 'index/media/setuptocat.jpg';
+
+octocats[93]= new Image(75, 75);
+octocats[93].src = 'index/media/shoptocat.png';
+
+octocats[94]= new Image(75, 75);
+octocats[94].src = 'index/media/skitchtocat.png';
+
+octocats[95]= new Image(75, 75);
+octocats[95].src = 'index/media/snowoctocat.png';
+
+octocats[96]= new Image(75, 75);
+octocats[96].src = 'index/media/socialite.jpg';
+
+octocats[97]= new Image(75, 75);
+octocats[97].src = 'index/media/spectrocat.png';
+
+octocats[98]= new Image(75, 75);
+octocats[98].src = 'index/media/spidertocat.png';
+
+octocats[99]= new Image(75, 75);
+octocats[99].src = 'index/media/spocktocat.png';
+
+octocats[100]= new Image(75, 75);
+octocats[100].src = 'index/media/steroidtocat.png';
+
+octocats[101]= new Image(75, 75);
+octocats[101].src = 'index/media/stormtroopocat.png';
+
+octocats[102]= new Image(75, 75);
+octocats[102].src = 'index/media/strongbadtocat.png';
+
+octocats[103]= new Image(75, 75);
+octocats[103].src = 'index/media/supportcat.png';
+
+octocats[104]= new Image(75, 75);
+octocats[104].src = 'index/media/swagtocat.png';
+
+octocats[105]= new Image(75, 75);
+octocats[105].src = 'index/media/thanktocat.png';
+
+octocats[106]= new Image(75, 75);
+octocats[106].src = 'index/media/total-eclipse-of-the-octocat.jpg';
+
+octocats[107]= new Image(75, 75);
+octocats[107].src = 'index/media/trekkie.png';
+
+octocats[108]= new Image(75, 75);
+octocats[108].src = 'index/media/twenty-percent-cooler-octocat.png';
+
+octocats[109]= new Image(75, 75);
+octocats[109].src = 'index/media/visionary.jpg';
+
+octocats[110]= new Image(75, 75);
+octocats[110].src = 'index/media/waldocat.png';
+
+octocats[111]= new Image(75, 75);
+octocats[111].src = 'index/media/wilson.jpg';
+
+octocats[112]= new Image(75, 75);
+octocats[112].src = 'index/media/xtocat.jpg';
